f177e06905c57405babc94e9c8612d0e19469b2e
Kaynan51 <kaynansilva205@gmail.com>
2024-10-07 23:30:17 -0300

    Teste

--- src/jogo/grupo3/turmaA/characters/hero/Hero.java
+++ src/jogo/grupo3/turmaA/characters/hero/Hero.java
@@ -1,5 +1,12 @@
 package jogo.grupo3.turmaA.characters.hero;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class Hero {
+    // __ __ __ _
+    // ___ ___ _____ / /____ __ _____ _/_/ ___ / /____ _____(_)__
+    // / _ `/ // (_-</ __/ _ `/ |/ / _ \ / -_) / _ \/ __/ _ `/ __/ / _ \
+    // \_, /\_,_/___/\__/\_,_/|___/\___/ \__/ \___/\__/\_,_/_/ /_/\___/
+    // /___/
 
 }
