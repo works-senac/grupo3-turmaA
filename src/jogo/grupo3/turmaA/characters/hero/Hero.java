2de0ef57d79220a32e7e5f71e421a971d230c8cc
Kaynan51 <kaynansilva205@gmail.com>
2024-10-31 19:00:45 -0200

    começo historia

--- src/jogo/grupo3/turmaA/characters/hero/Hero.java
+++ src/jogo/grupo3/turmaA/characters/hero/Hero.java
@@ -1,10 +1,22 @@
 package jogo.grupo3.turmaA.characters.hero;
 
-public class Hero {
-    // __ __ __ _
-    // ___ ___ _____ / /____ __ _____ _/_/ ___ / /____ _____(_)__
-    // / _ `/ // (_-</ __/ _ `/ |/ / _ \ / -_) / _ \/ __/ _ `/ __/ / _ \
-    // \_, /\_,_/___/\__/\_,_/|___/\___/ \__/ \___/\__/\_,_/_/ /_/\___/
-    // /___/
+import java.util.Scanner;
 
+public class Hero {
+    public static void main(String[] args) {
+        Scanner hero = new Scanner(System.in);
+        System.out.println("Insira seu nome: ");
+        String name = hero.next();
+        System.out.println(
+                name + "Ve sua vila infectada, porém como ainda era um jovem aprendiz de curandeiro ele acaba não conseguindo fazer nada.");
+        System.out.println("Seus amigos te chamam com urgência, você vai ou não?");
+        System.out.println("SIM = 1 ou NÃO = 2");
+        int decision1 = hero.nextInt();
+
+        if (decision1 == 1) {
+            System.out.println("Você corre com eles até sua casa, e se depara com uma cena lamentável.");
+        }
+
+        hero.close();
+    }
 }
