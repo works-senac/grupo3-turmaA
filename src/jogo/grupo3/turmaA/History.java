0a5c83af6b9387e03a5227b08155817560302299
raulrsoares <raulrodriguessoares@gmail.com>
2024-11-04 10:15:23 -0300

    feat: villain, class

--- src/jogo/grupo3/turmaA/History.java
+++ src/jogo/grupo3/turmaA/History.java
@@ -1,6 +1,7 @@
 package jogo.grupo3.turmaA;
 
 import jogo.grupo3.turmaA.characters.Hero;
+import jogo.grupo3.turmaA.characters.Villain;
 import jogo.grupo3.turmaA.utils.Utils;
 
 import java.util.Scanner;
@@ -17,7 +18,7 @@ public class History {
     }
 
     public void showGame() {
-        System.out.println("Antes de iniciarmos a sua jornada no incrivel mundo da pequena cidade de SilverHollow,");
+        System.out.println("Antes de iniciarmos a sua jornada no incrível mundo da pequena cidade de SilverHollow,");
         System.out.println("precisamos saber o nome que você escolhera para seu Herói.");
         String heroName = (String) Utils.verifyEntry("Qual sua escolha: ", scanner, "next");
         Utils.sleepTiming(1000);
@@ -27,6 +28,8 @@ public class History {
         Utils.limparTela(100);
         Hero hero = new Hero(heroName);
         hero.startHeroHistory();
+        Villain villain = new Villain("Ancião");
+        villain.startFinalBattle();
     }
 
     public void history1(String heroName) {
