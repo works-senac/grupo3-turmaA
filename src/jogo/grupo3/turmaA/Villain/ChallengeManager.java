caba269c397698227c74d322ded58e3a08f9aeca
Gustavo-oli <89321547+Gustavo-oli@users.noreply.github.com>
2024-11-07 23:10:05 -0300

    Delete src/jogo/grupo3/turmaA/Villain directory

--- src/jogo/grupo3/turmaA/Villain/ChallengeManager.java
+++
@@ -1,6 +0,0 @@
-class ChallengeManager {
-    public boolean executeRandomChallenge(String challengeLevel) {
-        // Simulando um desafio aleatório
-        return Math.random() > 0.5; // Retorna verdadeiro ou falso aleatoriamente
-    }
-}
