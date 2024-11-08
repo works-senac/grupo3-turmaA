cfe638071003c7915000701f2cdaebfdf84578f1
raulrsoares <raulrodriguessoares@gmail.com>
2024-11-08 00:36:56 -0300

    join project

--- src/jogo/grupo3/turmaA/characters/Villain.java
+++ src/jogo/grupo3/turmaA/characters/Villain.java
@@ -10,8 +10,12 @@ public class Villain {
     private final JavaChallengesManager challengeManager = new JavaChallengesManager();
     private final Scanner scanner = new Scanner(System.in);
     private int dragonHealth = 100; // Saúde do dragão
+    private int heroHealth = 100; // Saúde do Herói
+    public boolean historyFinal;
+    public int challengeLevel;
 
-    public Villain(String villainName) {
+    public Villain(String villainName, int challengeLevel) {
+        this.challengeLevel = challengeLevel;
         this.villainName = villainName;
     }
 
@@ -24,33 +28,32 @@ public class Villain {
         Utils.sleepTiming(1000);
         System.out.println("Transformo-me em um grande dragão, minhas escamas reluzindo como o fogo, e começo a rugir, fazendo a terra tremer!");
         Utils.sleepTiming(1000);
-
         battleWithHero();
     }
 
     private void battleWithHero() {
         boolean battleContinues = true;
-        int heroHealth = 100; // Saúde do herói
 
         while (battleContinues && (heroHealth > 0 && dragonHealth > 0)) {
-            System.out.println("=== Batalha em Curso ===");
+            System.out.println("=== Batalha Final em Curso ===");
             System.out.println("Saúde do Herói: " + heroHealth);
             System.out.println("Saúde do Dragão: " + dragonHealth);
             System.out.println("Escolha uma ação:");
-            System.out.println("╔════════════════════════════════╗");
-            System.out.println("║ 1. Lançar Ataque Mágico       ║");
-            System.out.println("║ 2. Usar Ataque Físico         ║");
-            System.out.println("║ 3. Lançar Ataque Especial      ║");
-            System.out.println("║ 4. Contra-atacar               ║");
-            System.out.println("║ 5. Usar Item                   ║");
-            System.out.println("╚════════════════════════════════╝");
-
+            System.out.println("""
+                    ╔════════════════════════════════╗
+                    ║ 1. Lançar Ataque Mágico        ║
+                    ║ 2. Usar Ataque Físico          ║
+                    ║ 3. Lançar Ataque Especial      ║
+                    ║ 4. Contra-atacar               ║
+                    ║ 5. Usar Item                   ║
+                    ╚════════════════════════════════╝
+                    """);
             int choice = (int) Utils.verifyEntry("Digite o número da sua escolha: ", scanner, "int");
 
             switch (choice) {
                 case 1 -> {
                     System.out.println(villainName + " lança uma magia poderosa!");
-                    if (executeChallenge("avançado")) {
+                    if (Utils.executarDesafio("difficult", challengeLevel, challengeManager, scanner)) {
                         System.out.println("A magia acerta o herói, que grita de dor!");
                         heroHealth -= 30; // Dano causado ao herói
                     } else {
@@ -85,9 +88,7 @@ public class Villain {
                         System.out.println("O herói se prepara e desvia do seu ataque!");
                     }
                 }
-                case 5 -> {
-                    useItem();
-                }
+                case 5 -> useItem();
                 default -> System.out.println("Escolha inválida. Tente novamente.");
             }
 
@@ -117,11 +118,13 @@ public class Villain {
 
     private void useItem() {
         System.out.println("Escolha um item para usar:");
-        System.out.println("╔════════════════════════════════╗");
-        System.out.println("║ 1. Poção de cura (Recupera 20 HP) ║");
-        System.out.println("║ 2. Bomba mágica (Causa 25 de dano ao herói) ║");
-        System.out.println("║ 3. Escudo mágico (Protege contra o próximo ataque do herói) ║");
-        System.out.println("╚════════════════════════════════╝");
+        System.out.println("""
+                ╔═════════════════════════════════════════════════════════════╗
+                ║ 1. Poção de cura (Recupera 20 HP)                           ║
+                ║ 2. Bomba mágica (Causa 25 de dano ao herói)                 ║
+                ║ 3. Escudo mágico (Protege contra o próximo ataque do herói) ║
+                ╚═════════════════════════════════════════════════════════════╝
+                """);
 
         int itemChoice = (int) Utils.verifyEntry("Digite o número da sua escolha: ", scanner, "int");
 
@@ -143,35 +146,6 @@ public class Villain {
     }
 
     private void endGame(boolean heroDefeated) {
-        if (heroDefeated) {
-            System.out.println("Com a vitória sobre o herói, eu, " + villainName + ", mantenho meu domínio sobre a vila.");
-            System.out.println("Os moradores continuam a sofrer com a doença, enquanto eu me escondo na escuridão.");
-        } else {
-            System.out.println("Após a derrota do dragão, (Usuário) encontrou um livro de magias entre os pertences do Ancião.");
-            Utils.sleepTiming(1000);
-            System.out.println("(Usuário) desesperado, leu a fórmula que prometia curar a doença que assolava a vila.");
-            Utils.sleepTiming(1000);
-            System.out.println("Retornando à vila, (Usuário) recitou as palavras mágicas, e todos os moradores começaram a se recuperar gradualmente.");
-            Utils.sleepTiming(1000);
-            System.out.println("A vila de Arvoredo voltou a ser um lugar de paz e alegria, e (Usuário) foi celebrado como um herói.");
-            Utils.sleepTiming(1000);
-            System.out.println("Sua coragem e determinação não apenas salvaram sua vila, mas também ensinaram a todos uma lição valiosa sobre esperança e perseverança.");
-            Utils.sleepTiming(1000);
-            System.out.println("Os moradores agora olham para (Usuário) com gratidão, sabendo que sua luta contra as trevas não foi em vão.");
-        }
-    }
-
-    private boolean executeChallenge(String challengeLevel) {
-        return challengeManager.executeRandomChallenge(challengeLevel);
-    }
-
-    public boolean tryAgain() {
-        System.out.println("Deseja tentar novamente?");
-        System.out.println("╔════════════════════════════════╗");
-        System.out.println("║ 1. Sim                        ║");
-        System.out.println("║ 0. Não                        ║");
-        System.out.println("╚════════════════════════════════╝");
-        int input = (int) Utils.verifyEntry("Qual será sua escolha: ", scanner, "int");
-        return input != 1;
+        historyFinal = !heroDefeated;
     }
 }
